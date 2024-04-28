19432649fed6758f01b5f19ba4a969223d7343f1
fl4mxr1 <88870330+fl4mxr1@users.noreply.github.com>
2024-04-28 08:10:44 +0200

    hhhhhhhhhhhhhhhhhhhh

--- src/script.js
+++ src/script.js
@@ -3,6 +3,8 @@ const pastTheHdr = document.querySelector("#thisIsPastTheHeaderHideTheScrollTip"
 const codingLanguagesItems_TT = document.querySelector("#codinglanguagesitems_tooltip")
 const abMeSec3 = document.querySelector(".am-sec3-grid")
 const cltItems = abMeSec3.querySelectorAll(".item")
+const sw = document.querySelector(".site-wrapper")
+const header = document.querySelector(".header")
 
 let selectedCLTItem = undefined
 
@@ -13,19 +15,25 @@ function updateSelectedCLTItem() {
 	}
 	codingLanguagesItems_TT.dataset.hidden = false
 	let boundingClient = selectedCLTItem.getBoundingClientRect()
-	codingLanguagesItems_TT.style.top = `${boundingClient.top}px`
-	codingLanguagesItems_TT.style.left = `${boundingClient.left}px`
-	console.log(selectedCLTItem)
+	codingLanguagesItems_TT.style.top = `${boundingClient.top + (boundingClient.height / 2) + 20}px`
+	codingLanguagesItems_TT.style.left = `${boundingClient.left + (boundingClient.width / 2) + 20}px`
 }
 setInterval(updateSelectedCLTItem, 1);
+setInterval(() => {
+	var scrollTop = sw.scrollTop
+	scrolldownTip.dataset.hidden = scrollTop > (header.clientHeight * 0.1)
+}, 1)
 
-let io = new IntersectionObserver((h) => {
-	scrolldownTip.dataset.hidden = h[0].isIntersecting
-})
-io.observe(pastTheHdr)
-
-cltItems.forEach(el => {
-	el.addEventListener("click", () => {
-		selectedCLTItem = el
+window.addEventListener('click', function(e){   
+	let h = undefined
+	cltItems.forEach(el => {
+		if (el.contains(e.target)){
+			console.log("hhhhhhhh")
+			h = el
+			aElement = el.getElementsByClassName("tooltip-wiki-link")[0]
+			console.log(aElement)
+		}
 	})
-});
+	selectedCLTItem = h
+	console.log(h)
+})
