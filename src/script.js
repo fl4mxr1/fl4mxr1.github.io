9e38eda97de0eb2eea5c386334c92ba3941fe383
fl4mxr1 <88870330+fl4mxr1@users.noreply.github.com>
2024-08-07 15:35:50 +0200

    about me got a glow up

--- src/script.js
+++
@@ -1,9 +0,0 @@
-const scrolldownTip = document.querySelector("#scrolldown-tip")
-const pastTheHdr = document.querySelector("#thisIsPastTheHeaderHideTheScrollTip")
-const header = document.querySelector(".header")
-const sw = document.querySelector(".site-wrapper")
-
-setInterval(() => {
-	var scrollTop = sw.scrollTop
-	scrolldownTip.dataset.hidden = scrollTop > (header.clientHeight * 0.1)
-}, 1)
